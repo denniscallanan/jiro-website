8bc65bacfda1bfa5405faf42c29a52827f948446
swssqewe12 <david@callanan.ie>
2017-09-17 18:18:21 +0200

    Improved hamburger menu functionality and looks

--- static/js/hamburger.js
+++ static/js/hamburger.js
@@ -3,14 +3,23 @@ function toggleMenu()
 	if (navMenu.classList.contains("open"))
 	{
 		navMenu.classList.remove("open");
+		navMenuBlack.classList.remove("visible");
 	}
 	else
 	{
 		navMenu.classList.add("open");
+		navMenuBlack.classList.add("visible");
 	}
 }
 
 function menuClick()
 {
 	navMenu.classList.remove("open");
+	navMenuBlack.classList.remove("visible");
+}
+
+navMenuBlack.onclick = function()
+{
+	navMenu.classList.remove("open");
+	navMenuBlack.classList.remove("visible");
 }
